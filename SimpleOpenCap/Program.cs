5a8f43659e35d6af0b02b489fc465bdad162490d
Kevin A <hello@cinderblockgames.com>
2022-08-11 22:55:49 -0400

    Moving swagger to / instead of /swagger.

--- SimpleOpenCap/Program.cs
+++ SimpleOpenCap/Program.cs
@@ -28,7 +28,11 @@ var app = builder.Build();
 if (enableSwagger)
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseCors();
